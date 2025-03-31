df2db28719c7c2d5c608558297d1bd424b56152b
PapryczkQ12 <pstryczek41@gmail.com>
2025-03-31 17:30:52 +0200

    Update id.js

--- assets/id.js
+++ assets/id.js
@@ -1,73 +1,33 @@
-
 var params = new URLSearchParams(window.location.search);
 
-document.querySelector(".login").addEventListener('click', () => {
-    toHome();
+function sendTo(url) {
+  location.href = `/${url}?` + params;
+}
+
+document.querySelectorAll(".bottom_element_grid").forEach((element) => {
+  element.addEventListener("click", () => {
+    sendTo(element.getAttribute("send"));
+  });
 });
 
-var welcome = "Dzień dobry!";
+function getMobileOperatingSystem() {
+  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
-var date = new Date();
-if (date.getHours() >= 18){
-    welcome = "Dobry wieczór!"
-}
-document.querySelector(".welcome").innerHTML = welcome;
+  if (/windows phone/i.test(userAgent)) {
+    return 1;
+  }
 
-function toHome(){
-    location.href = '/home?' + params;
-}
+  if (/android/i.test(userAgent)) {
+    return 2;
+  }
 
-var input = document.querySelector(".password_input");
-input.addEventListener("keypress", (event) => {
-    if (event.key === 'Enter') {
-        document.activeElement.blur();
-    }
-})
+  if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+    return 3;
+  }
 
-var dot = "•";
-var original = "";
-var eye = document.querySelector(".eye");
-
-input.addEventListener("input", () => {
-    var value = input.value.toString();
-    var char = value.substring(value.length - 1);
-    if (value.length < original.length){
-        original = original.substring(0, original.length - 1);
-    }else{
-        original = original + char;
-    }
-
-    if (!eye.classList.contains("eye_close")){
-        var dots = "";
-        for (var i = 0; i < value.length - 1; i++){
-            dots = dots + dot
-        }
-        input.value = dots + char;
-        delay(3000).then(() => {
-            value = input.value;
-            if (value.length != 0){
-                input.value = value.substring(0, value.length - 1) + dot
-            }
-        });
-        console.log(original)
-    }
-})
-
-function delay(time, length) {
-    return new Promise(resolve => setTimeout(resolve, time));
+  return 4;
 }
 
-eye.addEventListener('click', () => {
-    var classlist = eye.classList;
-    if (classlist.contains("eye_close")){
-        classlist.remove("eye_close");
-        var dots = "";
-        for (var i = 0; i < input.value.length - 1; i++){
-            dots = dots + dot
-        }
-        input.value = dots;
-    }else{
-        classlist.add("eye_close");
-        input.value = original;
-    }
-})
+if (getMobileOperatingSystem() == 2) {
+  document.querySelector(".bottom_bar").style.height = "70px";
+}
